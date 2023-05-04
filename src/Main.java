8aafc91b1afddd52c79a1961ce8a99b412588813
Sakib5858 <sakibafzal58@gmail.com>
2023-05-05 00:40:55 +0600

    2nd and 3rd commit

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,36 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        String str = JOptionPane.showInputDialog(null,"Enter your first name","Collect",JOptionPane.QUESTION_MESSAGE);
+//        String str = JOptionPane.showInputDialog(null,"Enter your first name","Collect",JOptionPane.QUESTION_MESSAGE);
+//
+//        String str1 = JOptionPane.showInputDialog(null,"Last name","Input 2",1);
+//
+//        String fs = str + " " + str1;
+//
+//        JOptionPane.showMessageDialog(null,fs+" Welcome","Details",2);
 
-        String str1 = JOptionPane.showInputDialog(null,"Last name","Input 2",1);
+        //-------------------------------------------------------------------------------
 
-        String fs = str + " " + str1;
+//        int choice = JOptionPane.showConfirmDialog(null,"Do you want to confirm ?","Confirmation",JOptionPane.YES_NO_CANCEL_OPTION);
+//
+//        if(choice == JOptionPane.YES_OPTION)
+//            System.out.println("Confirmed !!!!!");
+//        else if(choice == JOptionPane.NO_OPTION)
+//            System.out.println("You cancled this");
+//        else System.exit(0);
 
-        JOptionPane.showMessageDialog(null,fs+" Welcome","Details",2);
+        //---------------------------------------------------------------------------------------------
+
+        JFrame jf = new JFrame();
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //jf.setSize(500,300);
+        //jf.setLocationRelativeTo(null);
+        //jf.setLocation(500,300);
+        jf.setBounds(500,300,500,250);
+        jf.setTitle("Frame Demos");
+        jf.setResizable(false); //we cant change it size(can't be maximize)
+
+
     }
 }
