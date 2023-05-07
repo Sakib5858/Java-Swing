9c95d050a64941183927d8558c7bec739dc4e4ce
Sakib5858 <sakibafzal58@gmail.com>
2023-05-07 21:28:48 +0600

    6 Jlavel

--- src/JFrameIcon.java
+++ src/JFrameIcon.java
@@ -5,11 +5,22 @@ public class JFrameIcon extends JFrame{
 
     private ImageIcon im;
     private Container c;
+    private JLabel jl;
     public JFrameIcon(){
+
         im = new ImageIcon(getClass().getResource("gub logo.png"));
         this.setIconImage(im.getImage());
+
         c = this.getContentPane();
+        c.setLayout(null);
         c.setBackground(Color.lightGray);
+
+        jl = new JLabel();
+        jl.setBounds(100,1,500,350);
+        jl.setText("Enter your name: ");
+        c.add(jl);
+
+
     }
 
     public static void main(String[] args) {
@@ -18,7 +29,7 @@ public class JFrameIcon extends JFrame{
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.setBounds(500,300,500,250);
-
+        jf.setTitle("Green University");
 
     }
 }
